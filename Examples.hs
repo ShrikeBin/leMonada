f9fa9ea9c84161558b65b2f301ce0f5d57df91de
nskowron <nel.skowronek@gmail.com>
2025-06-05 19:52:50 +0200

    stochastic process example EXPONENTIAL TIME

--- Examples.hs
+++ Examples.hs
@@ -8,3 +8,12 @@ import Control.Applicative -- liftA2
 -- Knowing that the sum is no greater than 5
 example1 :: Prob
 example1 = (evalDist ((==2) . fst)) ((condDist ((<= 5) . uncurry (+))) (liftA2 (,) (die 6) (die 6)))
+
+-- Stochastic Processes
+example2 :: Int -> Dist Int
+example2 n = dedupeDist $ go 0 n
+    where
+        go pos 0 = return pos
+        go pos n = do
+            dir <- bernouli 0.5 (-1) 1
+            go (pos + dir) (n - 1)
