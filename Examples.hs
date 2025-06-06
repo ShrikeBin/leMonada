f1c66da1ce1aa81a01a395d4b3a996bf0bd64eb4
nskowron <nel.skowronek@gmail.com>
2025-06-06 14:00:56 +0200

    dist recognition - saving

--- Examples.hs
+++ Examples.hs
@@ -32,3 +32,22 @@ example3 n = dedupeDist $ do
 -- - polynomial time
 example4 :: Int -> Dist Int
 example4 n = fmap (\x -> 2 * x - n) (binomial n 0.5)
+
+-- We can try to check if a given Distribution
+-- is a known Distribution, and try to prove why we can
+-- write example4 using binomial dist.
+example5 :: Dist a -> String
+example5 (Dist []) = error "Empty Distribution"
+example5 dist
+    | isDist dist (bernouli x 0 1) = "Bernouli"
+    -- infinite - we're checking only a few first elements
+    | isDist dist ((Dist . take 10000 . unpackDist) (geometric x)) = "Geometric"
+    -- | isDist dist ((Dist . take 10000 . unpackDist) (negativeBinomial ))
+    -- finite
+    | isDist dist (uniform [0 .. n]) = "Uniform"
+    | isDist dist (binomial n (x ** (1 / fromIntegral n))) = "Binomial"
+    | otherwise = "Unknown"
+    where
+        isDist (Dist as) (Dist bs) = foldl (\q ((_, a), (_,b)) -> q && (a == b)) True (zip as bs)
+        ((_,x):xs) = unpackDist dist
+        n = length xs
